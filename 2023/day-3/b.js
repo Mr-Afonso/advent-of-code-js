dbf3e5fedb85be6a44c1ab99fbfba44ad8ebfd40
Miguel Afonso <mido@mobilae.com>
2023-12-04 22:05:29 +0100

    Mess

--- 2023/day-3/b.js
+++ 2023/day-3/b.js
@@ -1,34 +1,36 @@
-// const getInput = require('../../lib/getInput')
-const getInput = require('../../lib/getTestInput')
-
+const getInput = require('../../lib/getInput')
+// const getInput = require('../../lib/getTestInput')
+      
 let input = getInput(__dirname)
 const cleanInput = input.split("\n")
-
-// console.log(cleanInput)
-
+            
 // A gear is any * symbol that is adjacent to exactly two part numbers
 // Its gear ratio is the result of multiplying those two numbers together.
-
+  
 let sum = 0
-
+ 
 const findGear = (index, key) => {
-
+ 
   let numbers = []
-
+ 
   // right 
   let right = 1
   let rightNumber = ""
   while (!isNaN(cleanInput[index][key + right])) {
-    rightNumber += isNaN(cleanInput[index][key + right])
+    rightNumber += cleanInput[index][key + right]
     right++
   }
 
   if (rightNumber !== "" && !isNaN(rightNumber)) {
     numbers.push(rightNumber)
-  }
-
+  }  
+     
+  // if (!isNaN(cleanInput[index][key + 1]) && isNaN(cleanInput[index][key + 2])) {
+  //   numbers.push(`${cleanInput[index][key + 1]}`)
+  // } 
+  
   // left
-  let left = 1
+  let left = 1 
   let leftNumber = ""
   while (!isNaN(cleanInput[index][key - left])) {
     leftNumber += cleanInput[index][key - left]
@@ -39,6 +41,10 @@ const findGear = (index, key) => {
     numbers.push(leftNumber.split("").reverse().join(""))
   }
 
+  // if (!isNaN(cleanInput[index][key - 1]) && isNaN(cleanInput[index][key - 2])) {
+  //   numbers.push(`${cleanInput[index][key - 1]}`)
+  // }
+ 
   // top
   let top = 1
   let topNumberLeft = ""
@@ -50,18 +56,19 @@ const findGear = (index, key) => {
     if (topNumberLeft !== "" && !isNaN(topNumberLeft)) {
       if (!isNaN(cleanInput[index - 1][key])) {
         topNumberLeft = topNumberLeft.split("").reverse().join("") + cleanInput[index - 1][key]
+        numbers.push(topNumberLeft)
+      } else {
+        numbers.push(topNumberLeft.split("").reverse().join(""))
       }
-      numbers.push(topNumberLeft)
     }
-
-    top = 1
   }
 
+  let topRight = 1
   let topNumberRight = ""
   if (!isNaN(cleanInput[index - 1][key + 1])) {
-    while (!isNaN(cleanInput[index - 1][key + top])) {
-      topNumberRight += cleanInput[index - 1][key + top]
-      top++
+    while (!isNaN(cleanInput[index - 1][key + topRight])) {
+      topNumberRight += cleanInput[index - 1][key + topRight]
+      topRight++
     }
 
     if (topNumberRight !== "" && !isNaN(topNumberRight)) {
@@ -70,9 +77,18 @@ const findGear = (index, key) => {
       }
       numbers.push(topNumberRight)
     }
-
-    top = 1
   }
+
+  if (!isNaN(cleanInput[index - 1][key]) && !isNaN(cleanInput[index - 1][key - 1]) && !isNaN(cleanInput[index - 1][key + 1])) {
+    numbers.push(`${cleanInput[index - 1][key - 1]}${cleanInput[index - 1][key]}${cleanInput[index - 1][key + 1]}`)
+
+    numbers = [numbers[0], numbers[numbers.length-1]]
+  }
+
+  if (!isNaN(cleanInput[index - 1][key]) && isNaN(cleanInput[index - 1][key - 1]) && isNaN(cleanInput[index - 1][key + 1])) {
+    numbers.push(`${cleanInput[index - 1][key]}`)
+  }
+
   //down
   let down = 1
   let downNumberLeft = ""
@@ -89,16 +105,15 @@ const findGear = (index, key) => {
         numbers.push(downNumberLeft.split("").reverse().join(""))
       }
     }
-
-    down = 1
   }
 
+  let downRight = 1
   let downNumberRight = ""
   if (!isNaN(cleanInput[index + 1][key + 1])) {
-    while (!isNaN(cleanInput[index + 1][key + down])) {
-      downNumberRight += cleanInput[index + 1][key + down]
-      down++
-    }
+    while (!isNaN(cleanInput[index + 1][key + downRight])) {
+      downNumberRight += cleanInput[index + 1][key + downRight]
+      downRight++
+    }  
 
     if (downNumberRight !== "") {
       if (!isNaN(cleanInput[index + 1][key])) {
@@ -106,10 +121,17 @@ const findGear = (index, key) => {
       }
       numbers.push(downNumberRight)
     }
+  }  
 
-    down = 1
+  if (!isNaN(cleanInput[index + 1][key]) && !isNaN(cleanInput[index + 1][key - 1]) && !isNaN(cleanInput[index + 1][key + 1])) {
+    numbers.push(`${cleanInput[index + 1][key - 1]}${cleanInput[index + 1][key]}${cleanInput[index + 1][key + 1]}`)
+
+    numbers = [numbers[0], numbers[numbers.length-1]]
   }
 
+  if (!isNaN(cleanInput[index + 1][key]) && isNaN(cleanInput[index + 1][key - 1]) && isNaN(cleanInput[index + 1][key + 1])) {
+    numbers.push(`${cleanInput[index + 1][key]}`)
+  }
 
   return numbers
 
@@ -122,14 +144,23 @@ cleanInput.map((element, index) => {
 
     if (el === "*") {
       let arr = findGear(index, key)
+      // console.log(arr)
       if (arr.length == 2) {
         console.log(arr)
         sum += (Number(arr[0]) * Number(arr[1]))
       }
     }
 
-  })
+  }) 
 
 })
 
 console.log("Sum:", sum)
+
+// 75478173
+// 85790532
+// 74469599
+// 74463654
+
+// 75687867 - 
+// 75681922 - xxx
