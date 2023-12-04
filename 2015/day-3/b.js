d789946bbf053ace53fc78517eaf2711ee0a9cd6
Miguel Afonso <miguel@g2g-online.nl>
2023-12-04 08:30:29 +0100

    Solve 2015 - day 3.a

--- 2015/day-3/b.js
+++ 2015/day-3/b.js
@@ -2,6 +2,45 @@ const getInput = require('../../lib/getInput')
 // const getInput = require('../lib/getTestInput')
 
 let input = getInput(__dirname)
-const cleanInput = input.split("\n")
+const cleanInput = input.split("")
 
-console.log(cleanInput) 
+// console.log(cleanInput)
+
+let houses = []
+let now = [0, 0]
+
+let housesRoboSanta = []
+let nowRoboSanta = [0, 0]
+
+cleanInput.map((element) => {
+
+  if (element === "v") {
+    now = [now[0] + 1, now[1]]
+
+    houses.push(now.toString())
+  }
+
+  if (element === ">") {
+    now = [now[0], now[1] + 1]
+
+    houses.push(now.toString())
+  }
+
+  if (element === "<") {
+    now = [now[0], now[1] - 1]
+
+    houses.push(now.toString())
+  }
+
+  if (element === "^") {
+    now = [now[0] - 1, now[1]]
+
+    houses.push(now.toString())
+  }
+
+})
+
+console.log("all houses", houses.length)
+console.log("houses with at least one present", houses.filter(function (item, pos) {
+  return houses.indexOf(item) == pos;
+}).length)
