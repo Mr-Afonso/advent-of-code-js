faf1995de9b5f10804ef9c5deb97724d6f22bb05
Miguel Afonso <miguel@g2g-online.nl>
2023-12-15 10:26:36 +0100

    Solve part 1

--- 2023/day-15/a.js
+++ 2023/day-15/a.js
@@ -1,7 +1,35 @@
 const getInput = require('../../lib/getInput')
-// const getInput = require('../lib/getTestInput')
+// const getInput = require('../../lib/getTestInput')
 
 let input = getInput(__dirname)
-const cleanInput = input.split("\n")
+const cleanInput = input.split(",")
 
-console.log(cleanInput) 
+// console.log(cleanInput) 
+
+const anotherFunctionThatINeedToName = (string) => {
+
+  let value = 0
+
+  for (let index = 0; index < string.length; index++) {
+    // Determine the ASCII code for the current character of the string.
+    const asciiCode = string[index].charCodeAt()
+    // Increase the current value by the ASCII code you just determined.
+    value += asciiCode
+    // Set the current value to itself multiplied by 17.
+    value = value * 17
+    // Set the current value to the remainder of dividing itself by 256.
+    value = (value % 256)
+
+  }
+
+  return value
+
+}
+
+let value = []
+
+cleanInput.forEach((element) => {
+  value.push(anotherFunctionThatINeedToName(element))
+})
+
+console.log("Value:", value.reduce((a, b) => { return a + b }))
--- README.md
+++ README.md
@@ -26,7 +26,7 @@
 - [ ] Day 12
 - [ ] Day 13
 - [x] Day 14 - simple matrix day (4/10). Part 2 hunt for a pattern - Floyd's Tortoise and Hare Algorithm (Cycle Detection - loop detection) (8/10)
-- [ ] Day 15
+- [x] Day 15 - ASCII code day. charCodeAt() and modulo operation will be your friends (2/10).
 - [ ] Day 16
 - [ ] Day 17
 - [ ] Day 18
