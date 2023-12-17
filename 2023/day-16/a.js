d7edef4758bc4ccd80d0dea73c26916e6bd7e61b
Miguel Afonso <mido@mobilae.com>
2023-12-17 01:52:32 +0100

    Solving day 16, or not

--- 2023/day-16/a.js
+++ 2023/day-16/a.js
@@ -1,7 +1,146 @@
-const getInput = require('../../lib/getInput')
-// const getInput = require('../lib/getTestInput')
+// const getInput = require('../../lib/getInput')
+const getInput = require('../../lib/getTestInput')
 
 let input = getInput(__dirname)
-const cleanInput = input.split("\n")
+const cleanInput = input.split("\r\n")
 
-console.log(cleanInput) 
+// console.log(cleanInput) 
+
+let matrix = []
+let energizedMatrix = []
+
+for (let index = 0; index < cleanInput.length; index++) {
+    const element = cleanInput[index]
+    matrix[index] = []
+    energizedMatrix[index] = []
+
+    for (let z = 0; z < element.length; z++) {
+        const elementz = element[z]
+        matrix[index][z] = elementz
+        energizedMatrix[index][z] = "."
+    }
+}
+
+const matrixLength = matrix.length * matrix[0].length
+let count = 0
+
+const moveLightBeam = (x, y, direction) => {
+
+    // lets stop this madness
+    if (x < 0 || x > matrix.length - 1 || y < 0 || y > matrix[0].length-1 || count > matrixLength) {
+        // console.log("x",x)
+        // console.log("y",y)
+        // console.log("count",count)
+        // console.log('matrix.length', matrix.length)
+        // console.log('matrix[0].length ', matrix[0].length )
+        // console.log('matrixLength', matrixLength)
+        // console.log('========')
+        return false
+    }
+
+    energizedMatrix[x][y] = count
+    count++
+
+    if (direction === "right") {
+
+        if (matrix[x][y] === ".") {
+            return moveLightBeam(x, y + 1, "right")
+        }
+
+        if (matrix[x][y] === "-") {
+            return moveLightBeam(x, y + 1, "right")
+        }
+
+        if (matrix[x][y] === "|") {
+            return moveLightBeam(x - 1, y, "up"), moveLightBeam(x + 1, y, "down")
+        }
+
+        if (matrix[x][y] === "/") {
+            return moveLightBeam(x - 1, y, "up")
+        }
+
+        if (matrix[x][y] !== "-" && matrix[x][y] !== "|" && matrix[x][y] !== "/" && matrix[x][y] !== ".") {
+            return moveLightBeam(x + 1, y, "down")
+        }
+
+    }
+
+    if (direction === "left") {
+
+        if (matrix[x][y] === ".") {
+            return moveLightBeam(x, y - 1, "left")
+        }
+
+        if (matrix[x][y] === "-") {
+            return moveLightBeam(x, y - 1, "left")
+        }
+
+        if (matrix[x][y] === "|") {
+            return moveLightBeam(x + 1, y, "down"), moveLightBeam(x - 1, y, "up")
+        }
+
+        if (matrix[x][y] === "/") {
+            moveLightBeam(x + 1, y, "down")
+        }
+
+        if (matrix[x][y] !== "-" && matrix[x][y] !== "|" && matrix[x][y] !== "/" && matrix[x][y] !== ".") {
+            return moveLightBeam(x - 1, y, "up")
+        }
+
+    }
+
+    if (direction === "up") {
+
+        if (matrix[x][y] === ".") {
+            return moveLightBeam(x - 1, y, "up")
+        }
+
+        if (matrix[x][y] === "-") {
+            return moveLightBeam(x, y + 1, "right"), moveLightBeam(x, y - 1, "left")
+
+        }
+
+        if (matrix[x][y] === "|") {
+            return moveLightBeam(x - 1, y, "up")
+        }
+
+        if (matrix[x][y] === "/") {
+            return moveLightBeam(x, y + 1, "right")
+        }
+
+        if (matrix[x][y] !== "-" && matrix[x][y] !== "|" && matrix[x][y] !== "/" && matrix[x][y] !== ".") {
+            return moveLightBeam(x, y - 1, "left")
+        }
+
+    }
+
+    if (direction === "down") {
+
+        if (matrix[x][y] === ".") {
+            return moveLightBeam(x + 1, y, "down")
+        }
+
+        if (matrix[x][y] === "-") {
+            return moveLightBeam(x, y + 1, "right") || moveLightBeam(x, y - 1, "left")
+        }
+
+        if (matrix[x][y] === "|") {
+            return moveLightBeam(x + 1, y, "down")
+        }
+
+        if (matrix[x][y] === "/") {
+            return moveLightBeam(x, y - 1, "left")
+        }
+
+        if (matrix[x][y] !== "-" && matrix[x][y] !== "|" && matrix[x][y] !== "/" && matrix[x][y] !== ".") {
+            return moveLightBeam(x, y + 1, "right")
+        }
+
+    }
+
+}
+
+
+moveLightBeam(0, 0, "right")
+
+console.table(energizedMatrix)
--- README.md
+++ README.md
@@ -27,7 +27,7 @@
 - [ ] Day 13
 - [x] Day 14 - simple matrix day (4/10). Part 2 hunt for a pattern - Floyd's Tortoise and Hare Algorithm (Cycle Detection - loop detection) (8/10)
 - [x] Day 15 - ASCII code day. charCodeAt() and modulo operation will be your friends (2/10). Part 2 it is all about interpretation (5/10). Almost a rest day.
-- [ ] Day 16
+- [x] Day 16
 - [ ] Day 17
 - [ ] Day 18
 - [ ] Day 19
